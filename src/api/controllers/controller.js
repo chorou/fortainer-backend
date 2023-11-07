8e0fcbb378f04756f064bcb30d96678a66d8e02d
chorouq <chorouq1.laarouja@gmail.com>
2023-11-07 01:56:19 +0100

    Add controller for containers

--- src/api/controllers/controller.js
+++ src/api/controllers/controller.js
@@ -22,3 +22,65 @@ async function makeDockerRequest(apiPath, options = {}) {
     throw error;
   }
 }
+
+
+
+
+// Contrôleur pour les routes des conteneurs
+const containerController = {
+  getContainers: async (req, res) => {
+    try {
+      const containers = await makeDockerRequest('/containers/json');
+      res.json(containers);
+    } catch (error) {
+      res.status(error.response?.status || 500).json({ error: error.message });
+    }
+  },
+
+  createContainer: async (req, res) => {
+    try {
+      const { name, repoTag } = req.body;
+
+      if (!repoTag) {
+        throw new Error('RepoTag de l\'image Docker non spécifié.');
+      }
+
+      const imageIdResponse = await makeDockerRequest(`/images/${repoTag}/json`, {
+        method: 'get',
+      });
+
+      const imageId = imageIdResponse.Id;
+
+      const createContainerResponse = await makeDockerRequest('/containers/create', {
+        method: 'post',
+        data: {
+          name,
+          Image: imageId,
+        },
+      });
+
+      const startContainerResponse = await makeDockerRequest(`/containers/${createContainerResponse.Id}/start`, {
+        method: 'post',
+      });
+
+      res.json(createContainerResponse);
+    } catch (error) {
+      res.status(error.response?.status || 500).json({ error: error.message });
+    }
+  },
+
+  deleteContainer: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const removeContainerResponse = await makeDockerRequest(`/containers/${id}`, {
+        method: 'delete',
+      });
+
+      res.json(removeContainerResponse);
+    } catch (error) {
+      res.status(error.response?.status || 500).json({ error: error.message });
+    }
+  },
+};
+
