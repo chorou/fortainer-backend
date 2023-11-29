e93eae5db797ef5ffaac2fc6020ca5a599f2ad69
chorouq <chorouq1.laarouja@gmail.com>
2023-11-29 21:16:57 +0100

    Add docker Service:containers, images,and networks

--- src/api/services/dockerService.js
+++ src/api/services/dockerService.js
@@ -19,3 +19,40 @@ async function makeDockerRequest(apiPath, options = {}) {
     throw error;
   }
 }
+
+
+
+async function getContainers() {
+  try {
+    const containersData = await makeDockerRequest('/containers/json');
+    // Map the data to your model
+    const containers = containersData.map(container => new Container(container.Id, container.Names[0], container.State));
+    return containers;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getImages() {
+  try {
+    const imagesData = await makeDockerRequest('/images/json');
+    // Map the data to your model
+    const images = imagesData.map(image => new Image(image.Id, image.RepoTags[0], image.Size));
+    return images;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getNetworks() {
+  try {
+    const networksData = await makeDockerRequest('/networks');
+    // Map the data to your model
+    const networks = networksData.map(network => new Network(network.Id, network.Name, network.Driver));
+    return networks;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { getContainers, getImages, getNetworks };
