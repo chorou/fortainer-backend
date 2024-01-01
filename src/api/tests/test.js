538ba2a911292652461c98a731bb37e848279a1d
chorouq <chorouq1.laarouja@gmail.com>
2024-01-01 21:17:38 +0100

    Add test.js

--- src/api/tests/test.js
+++ src/api/tests/test.js
@@ -6,33 +6,3 @@ const { app } = require('../routes');
 const { expect } = chai;
 
 chai.use(chaiHttp);
-
-describe('Containers', () => {
-  it('should get a list of containers', async () => {
-    const res = await chai.request(app).get('/containers');
-    expect(res).to.have.status(200);
-    expect(res.body).to.be.an('array');
-  });
-
-  // Ajoutez d'autres tests pour les routes des conteneurs
-});
-
-describe('Images', () => {
-  it('should get a list of images', async () => {
-    const res = await chai.request(app).get('/images');
-    expect(res).to.have.status(200);
-    expect(res.body).to.be.an('array');
-  });
-
-  // Ajoutez d'autres tests pour les routes des images
-});
-
-describe('Networks', () => {
-  it('should get a list of networks', async () => {
-    const res = await chai.request(app).get('/networks');
-    expect(res).to.have.status(200);
-    expect(res.body).to.be.an('array');
-  });
-
-  // Ajoutez d'autres tests pour les routes des réseaux
-});
